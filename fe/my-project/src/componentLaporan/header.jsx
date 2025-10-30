27702c2d1a6fc8d32f3f5553ee469815c7858b65
Niel-D22 <danielwarouw01@gmail.com>
2025-10-30 12:15:28 +0800

    done Ui role pemilik/owner

--- fe/my-project/src/componentLaporan/header.jsx
+++ fe/my-project/src/componentLaporan/header.jsx
@@ -1,30 +1,33 @@
 import React from "react";
 import { IoIosSearch } from "react-icons/io";
-import { BiSolidFileExport } from "react-icons/bi";
+import BttnEkspor from "../kecilComponent/bttnEkspor";
 
-const header = () => {
+const header = ({ pilihStatus, setPilihStatus, CariProduk, setCariProduk }) => {
   return (
     <>
       <div className="w-full flex gap-9 text-[14px]">
         <div className="border-gray-300 bg-gray-50 border p-1 rounded-[8px] flex items-center gap-1.5 w-[300px]">
           <IoIosSearch className="text-2xl" />
-          <input type="text" placeholder="Cari Produk..." />
+          <input
+            value={CariProduk}
+            onChange={(e) => setCariProduk(e.target.value)}
+            className="px-2 w-full focus:outline-none "
+            type="text"
+            placeholder="Cari Produk..."
+          />
         </div>
         <div className="flex gap-106 ">
-              <select
-            
-                className=" p-1 border border-gray-300 bg-gray-50 w-[200px]   rounded-[10px] focus:outline-none focus:ring focus:ring-blue-400"
-              >
-                 <option value="" req>Status</option>
-                <option value="pemilik">Aman</option>
-                <option value="kasir">Menipis</option>
-                <option value="admin-gudang">Habis</option>
-              </select>
-
-              <div className=" p-1 border  rounded-[10px]  border-gray-300 bg-gray-50 w-[150px] gap-2 flex items-center justify-center ">
-                Ekspor Data
-                <BiSolidFileExport className="text-lg"/>
-                </div>
+          <select
+            value={pilihStatus}
+            onChange={(e) => setPilihStatus(e.target.value)}
+            className=" p-1 border border-gray-300 bg-gray-50 w-[200px]   rounded-[10px] focus:outline-none focus:ring focus:ring-blue-400"
+          >
+            <option value="Semua">Semua</option>
+            <option value="Aman">Aman</option>
+            <option value="Menipis">Menipis</option>
+            <option value="Habis">Habis</option>
+          </select>
+          <BttnEkspor />
         </div>
       </div>
     </>
