24447e104b6cda7c3479c52e7bacb78b7d0f78bf
Niel-D22 <danielwarouw01@gmail.com>
2025-11-04 11:00:57 +0800

    add:cors

--- be/app.py
+++ be/app.py
@@ -9,10 +9,13 @@ from models.product import Product
 from routes.auth.auth_routes import auth_bp
 from routes.products.product_routes import product_bp
 from utils.extensions import bcrypt, jwt, migrate
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+
+    CORS(app)
 
     bcrypt.init_app(app)
     db.init_app(app)
